cd23b95aea3cfba0947222f5fd4fc3c60245843b
Linh Phan <linhphanhust@gmail.com>
2018-04-05 20:37:21 +0700

    final state!

--- md5.c
+++ md5.c
@@ -54,12 +54,24 @@ char *password_to_md5(char *password) {
     return md5;
 }
 
+int finding_pass (int number, char *md5_input) {
+
+    int is_find = 0 ; 
+    char *password = number_to_password(number);
+    char *md5_password = password_to_md5(password);
+
+    if (strcmp(md5_input, md5_password) == 0) {
+        printf("YOUR PASSWORD: %s\n", password);
+        is_find = 1; 
+    }
+    free(password);
+    free(md5_password);
+    return is_find ;
+}
+
 int main() {
 
     char *md5_input = (char *) malloc (sizeof(char)*33) ;
-    char *md5_password ;
-    char *current_password ;
-
     int rank, ntasks;
     unsigned int i, j ;
 
@@ -72,57 +84,39 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank) ;
     MPI_Comm_size(MPI_COMM_WORLD, &ntasks) ;
 
-    unsigned int start, stop; // divide work to worker
-    int count, remain ;
+    unsigned int start, end; // divide work to worker
+    int count, remain ; 
+    double start_time, end_time ; // count time
 
     unsigned int current = 0 ; // number password decoded in current.
 
-    if(rank== 0) { // Master scan MD5 hash and send it to worker
+    if(rank== 0) { // Master scan MD5 hash
 
-        printf("\n ------- MD5 DECODE BY LINHPHAN -------\n") ;
+        printf("\n ------- MD5 DECODE BY LINHPHAN -------\n\n") ;
         printf("ENTER YOUR MD5 HASH: ") ;
         scanf("%s", md5_input) ;
-        for (j = 1; j < ntasks; j++)
-            MPI_Send(md5_input,33, MPI_CHAR, j, 101, MPI_COMM_WORLD);
     }
-    else {
-        MPI_Recv(md5_input, 33, MPI_CHAR, 0, 101, MPI_COMM_WORLD, &status);
-    }
-    
+
+    // Master broadcast md5 hash string to all worker
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(md5_input, 33, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+     start_time = MPI_Wtime() ;
+
     while(!is_find) { // stop find password when is_find = 1
 
         count = NLOOPS/ ntasks ; 
         start = current + rank*count ;
-        stop = start + count ;
+        end = start + count ;
 
-        for(i = start; i<stop; i++) {
-            current_password = number_to_password(i) ;
-            md5_password = password_to_md5(current_password) ;
-            
-            if(strcmp(md5_input, md5_password) == 0) {
-                printf("YOUR PASSWORD: %s\n", current_password) ;
-                printf("\n--------END PROGRAME ---------\n");
-                stop = start + NLOOPS ; // BREAK FOR LOOP.
-                is_find = 1 ; // BREAK WHILE LOOP.
-            }
-            free(current_password);
-            free(md5_password) ;
-        }
+        for(i = start; i<end; i++)
+            is_find += finding_pass(i, md5_input) ;
 
         if(rank==0) { // master caculate remaining password.
             remain = current + count*ntasks ;
-            for(i=remain; i<current + NLOOPS; i++) {
-                current_password = number_to_password(i);
-                md5_password = password_to_md5(current_password);
-                if (strcmp(md5_input, md5_password) == 0) {
-                    printf("YOUR PASSWORD: %s", current_password);
-                    printf("\n--------END PROGRAME ---------\n");
-                    is_find = 1;
-                }
-                free(current_password);
-                free(md5_password) ;
-            }
-
+            for(i=remain; i<current + NLOOPS; i++)
+                is_find += finding_pass(i, md5_input) ;
         }
         current += NLOOPS ;
         
@@ -130,15 +124,18 @@ int main() {
         MPI_Reduce(&is_find, &sum_is_find, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD ) ;
         is_find = sum_is_find ;
 
-        if (rank == 0) {
-            for (j = 1; j < ntasks; j++)
-                MPI_Send(&is_find, 1, MPI_INT, j, 102, MPI_COMM_WORLD);
-        }
-        else
-            MPI_Recv(&is_find, 1, MPI_INT, 0, 102, MPI_COMM_WORLD, &status);
+        MPI_Barrier(MPI_COMM_WORLD) ;
+        MPI_Bcast(&is_find, 1, MPI_INT, 0, MPI_COMM_WORLD) ;
+        MPI_Barrier(MPI_COMM_WORLD) ;
+
+    }
+    free(md5_input) ;
+    end_time = MPI_Wtime() ;
+    if(rank==0) {
+        printf("Total time: %f seconds !\n", end_time -start_time) ;
+        printf("\n--------END PROGRAME ---------\n");
     }
 
-    free(md5_input) ;
     MPI_Finalize() ;
 
     return 0;
@@ -149,7 +146,9 @@ int main() {
        MD5: 900150983cd24fb0d6963f7d28e17f72  ==> pass: abc
        MD5: 892da3d819056410c05bca7747d22735  ==> pass: linh
        MD5: b2157e7b2ae716a747597717f1efb7a0  ==> pass: abc12
-       MD5: c566c0fed4c02cf3751b4448df9be909  ==> pass: anh01
+       MD5: c566c0fed4c02cf3751b4448df9be909  ==> pass: nha01
        MD5: 39aed6dcb42493d547e209d195a4dddb  ==> pass: nhacsy
+       MD5: 02c2a68bffd54e229609ccc808cb45d7  ==> pass: it4865
+
 
     */
